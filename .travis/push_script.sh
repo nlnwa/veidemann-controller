2bb1b67f5b8f54f24ca9ceec79b3028203d53473
johnerikhalse <johnerikhalse@gmail.com>
2019-07-02 12:48:36 +0200

    Fix CI script

--- .travis/push_script.sh
+++ .travis/push_script.sh
@@ -3,8 +3,6 @@
 DOCKER_TAG=latest
 
 if [[ -n "$TRAVIS_TAG" || "$TRAVIS_BRANCH" == "master" && "$TRAVIS_EVENT_TYPE" == "push" ]]; then
-  docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
-
   if [[ -n "${TRAVIS_TAG}" ]]; then
     DOCKER_TAG=${TRAVIS_TAG}
   fi
